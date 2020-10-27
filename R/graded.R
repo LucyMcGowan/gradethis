893057703bec87dd0a094018e5a15ed710e9e968
Lucy McGowan <lucydagostino@gmail.com>
2020-10-27 12:13:07 -0400

    all ... to be passed (to allow for category)

--- R/graded.R
+++ R/graded.R
@@ -6,12 +6,12 @@
 #' @param message A character string of the message to be displayed.
 #' @param correct A boolean value of whether or not the checked code is correct.
 #' @export
-graded <- function(correct, message = NULL) {
+graded <- function(correct, message = NULL, ...) {
   chkm8_single_character(message)
   checkmate::expect_logical(correct, any.missing = FALSE, len = 1, null.ok = FALSE)
   
   structure(
-    list(message = message %||% "", correct = correct),
+    list(message = message %||% "", correct = correct, ...),
     class = "grader_graded"
   )
 }
