4a9f357b231d4fe1de09dd4e369e1b07bb048822
Lucy McGowan <lucydagostino@gmail.com>
2020-10-27 11:45:52 -0400

    solution -> mistake

--- R/category_mistake.R
+++ R/category_mistake.R
@@ -1,12 +1,12 @@
-category_mistake <- function(solution) {
-  if (is_infix(solution) | is.call(solution)) {
-    solution <- solution[[1]]
+category_mistake <- function(mistake) {
+  if (is_infix(mistake) | is.call(mistake)) {
+    mistake <- mistake[[1]]
   }
-  if (!is.character(solution)) solution <- deparse_to_string(solution)
+  if (!is.character(mistake)) mistake <- deparse_to_string(mistake)
   
   class_tbl <- tidycode::get_classifications(lexicon = "crowdsource", 
                                              include_duplicates = FALSE)
-  if (solution %in% class_tbl$func) {
-    class_tbl$classification[class_tbl$func == solution]
+  if (mistake %in% class_tbl$func) {
+    class_tbl$classification[class_tbl$func == mistake]
   }
 }
