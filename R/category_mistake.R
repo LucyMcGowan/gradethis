202ee31a2f71d30b858382988f00d2556a0c4bc5
Lucy McGowan <lucydagostino@gmail.com>
2020-10-27 12:12:17 -0400

    pass lexicon, change default

--- R/category_mistake.R
+++ R/category_mistake.R
@@ -1,10 +1,10 @@
-category_mistake <- function(mistake) {
+category_mistake <- function(mistake, lexicon = "leeklab") {
   if (is_infix(mistake) | is.call(mistake)) {
     mistake <- mistake[[1]]
   }
   if (!is.character(mistake)) mistake <- deparse_to_string(mistake)
   
-  class_tbl <- tidycode::get_classifications(lexicon = "crowdsource", 
+  class_tbl <- tidycode::get_classifications(lexicon = lexicon, 
                                              include_duplicates = FALSE)
   if (mistake %in% class_tbl$func) {
     class_tbl$classification[class_tbl$func == mistake]
